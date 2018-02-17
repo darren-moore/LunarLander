82d88f8cf9537df6931b9eeec9a177f12788f53d
Darren <moodarren@gmail.com>
2018-02-17 14:48:26 -0500

    Shader class.

---
+++ LunarLander/Shader.h
@@ -0,0 +1,29 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+#include <string> 
+#include <sstream>
+#include <fstream>
+
+class Shader {
+public:
+	unsigned int ID;
+
+	Shader(GLchar const* vertexPath, GLchar const* fragmentPath);
+
+	void use();
+
+	void setBool(std::string const &name, bool value) const;
+	void setInt(std::string const &name, int value) const;
+	void setFloat(std::string const &name, float value) const;
+	void setMat4(std::string const &name, glm::mat4 value) const;
+
+};
+
+#endif //SHADER_H
--- LunarLander/main.cpp
+++ LunarLander/main.cpp
@@ -10,6 +10,8 @@
 #include <sstream>
 #include <fstream>
 
+#include "Shader.h"
+
 using namespace std;
 
 // Window resize callback for dynamic viewport
@@ -45,77 +47,15 @@ int main(){
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	
 	// Window is now set up.
-
+	
 	// Compile and link shaders
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
-	int  shaderSuccess;
-	char shaderInfoLog[512];
-	{
-		// Vertex shader
-		const char* vertexShaderSource;
-		std::fstream vfile("vertShader.vert");
-		std::stringstream vbuffer;
-		vbuffer << vfile.rdbuf();
-		string vString = vbuffer.str();
-		vertexShaderSource = vString.c_str();
-		vfile.close();
-		
-		unsigned int vertexShader;
-		vertexShader = glCreateShader(GL_VERTEX_SHADER);
-		glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-		glCompileShader(vertexShader);
-
-		// Check for shader compilation errors
-		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &shaderSuccess);
-		if (!shaderSuccess) {
-			glGetShaderInfoLog(vertexShader, 512, NULL, shaderInfoLog);
-			cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << shaderInfoLog << endl;
-		}
-
-
-		// Fragment shader
-		const char* fragmentShaderSource;
-		std::ifstream ffile("fragShader.frag");
-		std::stringstream fbuffer;
-		fbuffer << ffile.rdbuf();
-		string fString = fbuffer.str();
-		fragmentShaderSource = fString.c_str();
-		ffile.close();
-
-		unsigned int fragmentShader;
-		fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-		glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-		glCompileShader(fragmentShader);
-
-		// Check for shader compilation errors
-		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &shaderSuccess);
-		if (!shaderSuccess) {
-			glGetShaderInfoLog(fragmentShader, 512, NULL, shaderInfoLog);
-			cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << shaderInfoLog << endl;
-		}
-
-		// Attach shaders to shader program
-		
-		glAttachShader(shaderProgram, vertexShader);
-		glAttachShader(shaderProgram, fragmentShader);
-		glLinkProgram(shaderProgram);
-
-		// Check for shader linking errors
-		glGetShaderiv(shaderProgram, GL_LINK_STATUS, &shaderSuccess);
-		if (!shaderSuccess) {
-			glGetProgramInfoLog(shaderProgram, 512, NULL, shaderInfoLog);
-			cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << shaderInfoLog << endl;
-		}
-
-		glDeleteShader(vertexShader);
-		glDeleteShader(fragmentShader);
-	}
+	Shader shader("vertShader.vert", "fragShader.frag");
 
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f,  0.5f, 0.0f
+		// positions         // colors
+		0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+		0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
 	};
 
 	// Vertex array object
@@ -129,19 +69,32 @@ int main(){
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	
 
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
+		shader.use();
 
+		glm::mat4 transform = glm::mat4(1.0f);
+		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0, 1.0, 0.0));
+
+		// Set shader uniforms
+		float timeValue = glfwGetTime();
+		float greenValue = (sin(timeValue) / 2.0f) + .5f;
+		shader.setFloat("gv", greenValue);
+		shader.setMat4("transform", transform);
+		
+		// Render stuff
+		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window); // double buffer!
